637bcbcee3aef5bc099cd4f02ae4b3bc18cec776
Nazar Blyzniuk <nazar1998777718@gmail.com>
2022-07-02 16:42:56 +0300

    DOM of main components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './componens/Header'; //included Header.module.css
-import Sidebar from './componens/Sidebar'; //included Sidebar.module.css
-import Profile from './componens/Profile'; //included Profile.module.css
+import Header from './componens/Header/Header'; //included Header.module.css
+import Sidebar from './componens/Sidebar/Sidebar'; //included Sidebar.module.css
+import Profile from './componens/Profile/Profile'; //included Profile.module.css
 import './index.css'; //only css grid settings
 
 const App = () => {
